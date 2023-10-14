9624b543e373d088ea4faf84699265d839bb0a0e
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-10-14 22:35:22 +0500

    Task4

---
+++ Tyuiu.SolievAH.Sprint1.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolievAH.Sprint1.Task4.V28.Lib;
+
+namespace Tyuiu.SolievAH.Sprint1.Task4.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(0, 0);
+
+            Assert.AreEqual(1, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint1.Task4.V28/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SolievAH.Sprint1.Task4.V28.Lib;
+
+namespace Tyuiu.SolievAH.Sprint1.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x, y через enter: ");
+            double x, y;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Солиев А. Х. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Солиев Алижон Хотамжонович | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу,  которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("*  вычисляет результат по формуле и печатает его на экране                *");
+            Console.WriteLine("*  Формула: cos(60*pi / 2) / Exp(2 * x + y)                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(x +" "+ y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.Calculate(x, y);
+            Console.WriteLine(Math.Round(res, 3));
+
+
+            Console.ReadKey();
+        }
+    }
+}
