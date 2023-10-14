1b892e2e80ab6391821cec8023c51b026390ea30
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-10-14 22:04:48 +0500

    Task3

---
+++ Tyuiu.SolievAH.Sprint1.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolievAH.Sprint1.Task3.V14.Lib;
+
+namespace Tyuiu.SolievAH.Sprint1.Task3.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.ReverseNumber(123);
+
+
+            Assert.AreEqual(res, 321);
+        }
+    }
+}
--- Tyuiu.SolievAH.Sprint1.Task3.V14/Program.cs
+++ Tyuiu.SolievAH.Sprint1.Task3.V14/Program.cs
@@ -3,39 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.SolievAH.Sprint1.Task2.V16.Lib;
+using Tyuiu.SolievAH.Sprint1.Task3.V14.Lib;
 
-namespace Tyuiu.SolievAH.Sprint1.Task2.V16
+namespace Tyuiu.SolievAH.Sprint1.Task3.V14
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите радиус круга: ");
-            int value;
-            value = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите трёхзначное число: ");
+            int number;
+            number = int.Parse(Console.ReadLine());
             DataService ds = new DataService();
             Console.Title = "Спринт #1 | Выполнил: Солиев А. Х. | СМАРТб-23-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
             Console.WriteLine("* Выполнил: Солиев Алижон Хотамжонович | СМАРТб-23-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычислит примерный периметр круга.          *");
-            Console.WriteLine("* p=2*PI*R                                                                *");
+            Console.WriteLine("* Написать программу, которая позволяет определить число, полученное      *");
+            Console.WriteLine("* выписыванием в обратном порядке цифр заданного трехзначного числа       *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine(value);
+            Console.WriteLine(number);
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            double res = ds.CalculatePerimetrCircle(value);
-            Console.WriteLine(Math.Round(res, 3));
+            double res = ds.ReverseNumber(number);
+            Console.WriteLine(res);
 
 
             Console.ReadKey();
