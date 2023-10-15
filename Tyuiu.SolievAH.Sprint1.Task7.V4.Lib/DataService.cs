8ab9036ac53b42a22c7e8709790f02fa00627a41
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-10-15 13:48:08 +0500

    Task7

---
+++ Tyuiu.SolievAH.Sprint1.Task7.V4.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SolievAH.Sprint1.Task7.V4.Lib
+{
+    public class DataService : ISprint1Task7V4
+    {
+        public double Calculate(double x, double y)
+        {
+            double a, a1, z;
+            a = x - (y / (x + x * x / 4));
+            a1 = y - Math.Sqrt(Math.Abs(x));
+            z = Math.Log10(Math.Abs(a1 * a));
+            return Math.Round(z, 3);
+            //−0.247117924 ПРИМЕРНЫЙ ОТВЕТ
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint1.Task7.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SolievAH.Sprint1.Task7.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SolievAH.Sprint1.Task7.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed68b67f-cc54-4c2d-990a-330ccae235d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
